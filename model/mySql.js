d705876bed732fd6681bff3fc60b07909e54e6d6
Siri <sirilakku01@gmail.com>
2022-05-24 15:45:36 -0600

    MySql Database connection

---
+++ model/mySql.js
@@ -0,0 +1,19 @@
+const mySql = require("mysql");
+
+let connectionString = mySql.createConnection({
+  host: "localhost",
+  //port : 3306,
+  user: "root",
+  password: "",
+  //database: "Database"
+});
+
+connectionString.connect(function (err) {
+  if (err) {
+    console.log("Database connection Error");
+  } else {
+    console.log("MySql Database Connected!");
+  }
+ });
+
+module.exports = connectionString;
