0152dbafdd0047a8167aa6cee0b9d1d5a7993f86
lmifflen <lmifflen@gmail.com>
2022-05-26 15:34:31 -0600

    added socket.io server for messenger

--- server.js
+++ server.js
@@ -10,4 +10,34 @@ function echoPortNumber(){
     console.log(`Listening on port ${PORT}`)
 }
 
-app.listen(PORT,echoPortNumber)
+//creating socket server for messenger
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+
+io.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+io.on("connection", (socket) => {
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    socket.leave(roomId);
+  });
+});
+
+
+server.listen(PORT, echoPortNumber);
