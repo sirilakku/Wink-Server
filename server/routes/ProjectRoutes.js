2b57d5cc6ed74a6a23e6461224bd6a0f8dd67774
Siri <sirilakku01@gmail.com>
2022-06-03 09:43:16 -0600

    Added monthlycalendar route

--- server/routes/ProjectRoutes.js
+++ server/routes/ProjectRoutes.js
@@ -1,5 +1,5 @@
 let express = require('express');
-const { welcome, login } = require('../../model/functions');
+const { welcome, login, monthlyCalendar} = require('../../model/functions');
 let router = express.Router();
 let database = require('../../model/mySql')
 
@@ -8,6 +8,8 @@ let database = require('../../model/mySql')
 router.get('/', welcome);
 
 router.get('/login', login)
+
+ router.get('/monthlyCalendar', monthlyCalendar)
 
 // router.post('/login', ()=>{
 
