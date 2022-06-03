2aca07802cb3928b3b94c73ed126ed4c91f30834
Siri <sirilakku01@gmail.com>
2022-06-03 09:41:36 -0600

    Added port number

--- model/mySql.js
+++ model/mySql.js
@@ -2,18 +2,20 @@ const mySql = require("mysql");
 
 let connectionString = mySql.createConnection({
   host: "localhost",
-  //port : 3306,
+  port : 3306,
   user: "root",
-  password: "",
+  password: ""
   //database: "Database"
 });
 
-connectionString.connect(function (err) {
+connectionString.connect(
+  function (err) {
   if (err) {
     console.log("Database connection Error");
   } else {
     console.log("MySql Database Connected!");
   }
- });
+ }
+ );
 
 module.exports = connectionString;
