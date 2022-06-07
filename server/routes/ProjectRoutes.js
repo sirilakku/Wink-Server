f799c87014ce618cef7dc7be23384bd3bc62cc8f
lmifflen <lmifflen@gmail.com>
2022-06-07 14:15:46 -0600

    added some Auth logic

--- server/routes/ProjectRoutes.js
+++ server/routes/ProjectRoutes.js
@@ -1,24 +1,47 @@
-const userByName  = require("../../model/userModel")
+const userByName = require("../../model/userModel");
 
-let express = require('express');
-const { welcome, login, monthlyCalendar, user} = require('../../model/functions');
+
+let express = require("express");
+const {
+  welcome,
+  login,
+  monthlyCalendar,
+  user,
+} = require("../../model/functions");
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', welcome);
+router.get("/", welcome);
 
- //router.get('/login', login)
+//router.get('/login', login)
 
-router.get('/monthlyCalendar', monthlyCalendar)
+router.get("/monthlyCalendar", monthlyCalendar);
 
 router.post("/login", async (req, res) => {
-    const user = req.body.username
+  const user = req.body.username;
+  const password = req.body.password;
 
-    const username = await userByName(user)
-
-    console.log('Hello username', username)
-    console.log('Hello user', user)
-    res.send(user.firstname)
-})
+  const username = await userByName(user);
+  if (
+    username.username === user &&
+    username.password === password &&
+    username.inactive === false
+  ) {
+    console.log("Hello username", username);
+    console.log("Hello password", password, username.password);
+    res.json({
+      firstname: username.firstname,
+      lastname: username.lastname,
+      username: username.username,
+      password: username.password,
+      inactive: username.inactive,
+      userID: username.idUser,
+    });
+  } else {
+    res.json({
+      message: "Invalid username or password",
+    });
+  }
+});
 
 module.exports = router;
