1d420492a917fb9ae1e65f5c0fafde3fa5d6f0bf
Siri <sirilakku01@gmail.com>
2022-06-08 19:02:05 -0600

    Created function to fetch user by store ID

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,9 @@
 //import { PrismaClient } from '@prisma/client';
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, Prisma } = require('@prisma/client');
 
+const user = require('@prisma/client')
+
+// using `prisma` in your application to read and write data in DB
 const prisma = new PrismaClient()
 
 
@@ -28,4 +31,27 @@ let userByName = async (username) => {
         })
         return userByName
     }
-module.exports=userByName
+
+let userByStoreID = async(username) => {
+
+// let userData = await prisma.user.count({
+//   where : {
+//     username : username,
+//     },
+// })
+//  console.log("Result is ", userData)
+//  return userData
+//let usern = username
+const result = await prisma.$queryRaw`SELECT idUser,username FROM user`
+
+  //const result = await prisma.$queryRaw`SELECT * FROM user WHERE user.idUser = {username}`
+  // let result = await prisma.user.$queryRaw `select * from user` ({where : {user.idUser : username} IN (${Prisma.join(usernameByProID)})});
+
+  // let queryForProfileId = await prisma.user.$queryRaw `select UserProfile_idUserProfile from userprivileges`
+  // let usernameByProID = await prisma.user.$queryRaw `select userprofile.name from userprofile WHERE username IN (${Prisma.join(queryForProfileId)})`;
+  return result
+}
+
+
+
+module.exports={userByName, userByStoreID}
