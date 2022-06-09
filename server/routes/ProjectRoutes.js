472f7d60cf13f4cf9ad74dcc4b414ea18903cea7
Siri <sirilakku01@gmail.com>
2022-06-08 19:01:15 -0600

    Created userprofile end point

--- server/routes/ProjectRoutes.js
+++ server/routes/ProjectRoutes.js
@@ -1,4 +1,5 @@
-const userByName  = require("../../model/userModel")
+const {userByName, userByStoreID}  = require("../../model/userModel")
+ 
 
 let express = require('express');
 const { welcome, login, monthlyCalendar, user} = require('../../model/functions');
@@ -13,12 +14,19 @@ router.get('/monthlyCalendar', monthlyCalendar)
 
 router.post("/login", async (req, res) => {
     const user = req.body.username
-
     const username = await userByName(user)
-
-    console.log('Hello username', username)
+    //console.log('Hello username', username)
     console.log('Hello user', user)
     res.send(user.firstname)
 })
 
-module.exports = router;
+router.get('/userprofile', async(req, res) =>{
+    const user = req.body.username
+    //console.log('Hello user in userprofile', user)
+    const userNProfile = await userByStoreID(user)
+    console.log("user name with profile", userNProfile)
+    res.send(userNProfile.firstname)
+    
+})
+
+    module.exports = router;
