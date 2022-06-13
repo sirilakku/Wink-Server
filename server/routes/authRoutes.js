0a8cab798684a9974eaded7e70e3cedd9a1c48f5
lmifflen <lmifflen@gmail.com>
2022-06-13 11:48:27 -0600

    succesfully logging out of the backend

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,61 +5,76 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const { userByName } = require("../../model/userModel");
 
-
 passport.use(
-    new LocalStrategy(async function (username, password, done) {
-      try {
-        const user = await userByName(username);
-        if (!user) {
-          return done(null, false);
-        }
-        const passwordsMatch = (password === user.password);
-        if (!passwordsMatch) {
-          return done(null, false);
-        }
-        // const userToSend = user.toObject();
-        const userToSend = {username: user.username, firstname: user.firstname, lastname: user.lastname, inactive: user.inactive, id: user.idUser};
-        // delete userToSend.password;
-        console.log("user after deleting password", userToSend);
-        return done(null, userToSend);
-      } catch (error) {
-        return done(error, null);
-      }
-    })
-  );
-
-passport.serializeUser(function (user, done) {
-    console.log("passport wants to store this user in a cookie", user);
-    done(null, user.username);
-  });
-  
-  passport.deserializeUser(async function (username, done) {
-    console.log("passport is trying to recover the user from the cookie", username);
+  new LocalStrategy(async function (username, password, done) {
     try {
       const user = await userByName(username);
       if (!user) {
-        done(new Error("User not found or deleted"));
-        return;
+        return done(null, false);
       }
-      done(null, user);
+      const passwordsMatch = password === user.password;
+      if (!passwordsMatch) {
+        return done(null, false);
+      }
+      // const userToSend = user.toObject();
+      const userToSend = {
+        username: user.username,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        inactive: user.inactive,
+        id: user.idUser,
+      };
+      // delete userToSend.password;
+      console.log("user after deleting password", userToSend);
+      return done(null, userToSend);
     } catch (error) {
-      done(error);
+      return done(error, null);
     }
-  });
-  
-  router.post("/login", passport.authenticate("local"), (req, res) => {
-    res.send(req.user);
-  });
+  })
+);
 
-  router.get("/logout", (req, res) => {
-    req.logout();
-    res.send("successfully logged out");
+passport.serializeUser(function (user, done) {
+  console.log("passport wants to store this user in a cookie", user);
+  done(null, user.username);
+});
+
+passport.deserializeUser(async function (username, done) {
+  console.log(
+    "passport is trying to recover the user from the cookie",
+    username
+  );
+  try {
+    const user = await userByName(username);
+    if (!user) {
+      done(new Error("User not found or deleted"));
+      return;
+    }
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.send(req.user);
+});
+
+router.get("/logout", (req, res) => {
+  req.logout(req.user, err => {
+    if(err) return next(err);
+    res.redirect("/");
   });
-  
-  router.get("/loggedInUser", (req, res) => {
-    const userToSend = {username: req.user.username, firstname: req.user.firstname, lastname: req.user.lastname, inactive: req.user.inactive, id: req.user.idUser};
-    res.send(userToSend);
-  });
-  
-  module.exports = router;
-  
+});
+
+router.get("/loggedInUser", (req, res) => {
+  const userToSend = {
+    username: req.user.username,
+    firstname: req.user.firstname,
+    lastname: req.user.lastname,
+    inactive: req.user.inactive,
+    id: req.user.idUser,
+  };
+  res.send(userToSend);
+});
+
+module.exports = router;
