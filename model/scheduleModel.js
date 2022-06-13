1ca4e278bfde6bd5affaaca2a821deda5839e22e
Inyoung Park <rkduddl1rl@gmail.com>
2022-06-13 15:54:35 -0600

    ScheduleModel - get opening hours are created

--- model/scheduleModel.js
+++ model/scheduleModel.js
@@ -1,29 +1,27 @@
 const { PrismaClient } = require("@prisma/client");
 // using `prisma` in your application to read and write data in DB
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const createSchedules =async()=>{
-
-    // const schedules = await prisma.schedule.createMany({
-    
-    // data:[
-    //   {workCode: "working",startTime:"2022-06-13 09:00:00", endTime:"2022-6-13 17:00:00", privilegesId:12},
-    //   {workCode: "working",startTime:"2022-06-14 09:00:00", endTime:"2022-6-14 17:00:00", privilegesId:12},
-    //   {workCode: "working",startTime:"2022-06-15 09:00:00", endTime:"2022-06-15 09:00:00", privilegesId:12},
-    //   {workCode: "working",startTime:"2022-06-16 09:00:00", endTime:"2022-06-14 09:00:00", privilegesId:12},
-    //   {workCode: "working",startTime:"2022-06-13 09:30:00", endTime:"2022-06-13 13:00:00", privilegesId:15},
-    //   {workCode: "working",startTime:"2022-06-14 10:00:00", endTime:"2022-06-14 17:00:00", privilegesId:15},
-    //   {workCode: "working",startTime:"2022-06-16 09:00:00", endTime:"2022-06-14 09:00:00", privilegesId:15},
-    //   {workCode: "working",startTime:"2022-06-14 09:00:00", endTime:"2022-06-14 17:00:00", privilegesId:17},
-    //   {workCode: "working",startTime:"2022-06-16 09:00:00", endTime:"2022-06-16 12:00:00", privilegesId:17},
-    //   {workCode: "working",startTime:"2022-06-13 09:00:00", endTime:"2022-06-13 17:00:00", privilegesId:23},
-    //   {workCode: "working",startTime:"2022-06-15 09:00:00", endTime:"2022-06-15 17:00:00", privilegesId:23},
-    //   {workCode: "vacation",startTime:"2022-06-15 00:00:00", endTime:"2022-06-25 23:59:59", privilegesId:74},
-    //   {workCode: "working",startTime:"2022-06-26 13:00:15", endTime:"2022-06-26 17:00:00", privilegesId:74},
-    // ]
-// })
+const createSchedules = async () => {
+  // const schedules = await prisma.schedule.createMany({
+  // data:[
+  //   {workCode: "working",startTime:"2022-06-13 09:00:00", endTime:"2022-6-13 17:00:00", privilegesId:12},
+  //   {workCode: "working",startTime:"2022-06-14 09:00:00", endTime:"2022-6-14 17:00:00", privilegesId:12},
+  //   {workCode: "working",startTime:"2022-06-15 09:00:00", endTime:"2022-06-15 09:00:00", privilegesId:12},
+  //   {workCode: "working",startTime:"2022-06-16 09:00:00", endTime:"2022-06-14 09:00:00", privilegesId:12},
+  //   {workCode: "working",startTime:"2022-06-13 09:30:00", endTime:"2022-06-13 13:00:00", privilegesId:15},
+  //   {workCode: "working",startTime:"2022-06-14 10:00:00", endTime:"2022-06-14 17:00:00", privilegesId:15},
+  //   {workCode: "working",startTime:"2022-06-16 09:00:00", endTime:"2022-06-14 09:00:00", privilegesId:15},
+  //   {workCode: "working",startTime:"2022-06-14 09:00:00", endTime:"2022-06-14 17:00:00", privilegesId:17},
+  //   {workCode: "working",startTime:"2022-06-16 09:00:00", endTime:"2022-06-16 12:00:00", privilegesId:17},
+  //   {workCode: "working",startTime:"2022-06-13 09:00:00", endTime:"2022-06-13 17:00:00", privilegesId:23},
+  //   {workCode: "working",startTime:"2022-06-15 09:00:00", endTime:"2022-06-15 17:00:00", privilegesId:23},
+  //   {workCode: "vacation",startTime:"2022-06-15 00:00:00", endTime:"2022-06-25 23:59:59", privilegesId:74},
+  //   {workCode: "working",startTime:"2022-06-26 13:00:15", endTime:"2022-06-26 17:00:00", privilegesId:74},
+  // ]
+  // })
   // return schedules
-}
+};
 
 const getAllSchedulesByStore = async (storeId) => {
   const allEmployees = await prisma.userprivileges.findMany({
@@ -35,8 +33,7 @@ const getAllSchedulesByStore = async (storeId) => {
       user: { select: { firstname: true, lastname: true } },
     },
   });
-//   console.log("result", allEmployees);
-
+  //   console.log("result", allEmployees);
 
   const res = [];
   allEmployees.map((emp) => {
@@ -48,9 +45,18 @@ const getAllSchedulesByStore = async (storeId) => {
     };
     res.push(dataObj);
   });
-//   console.log(res);
+  //   console.log(res);
   return res;
 };
 
-createSchedules();
+// createSchedules();
+
+const getStoreHours = async (storeId) => {
+  // const storeHours = await prisma.storeopeninghours.findMany();
+  const storeHours = await prisma.storeopeninghours.findMany({
+    where: { Store_idStore: 1 },
+  });
+  console.log("store hours", storeHours);
+};
+
 module.exports = getAllSchedulesByStore;
