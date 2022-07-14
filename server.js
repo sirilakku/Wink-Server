819e692eb625da964b4c6364092cf8d824f015cd
lmifflen <lmifflen@gmail.com>
2022-07-14 17:39:42 -0600

    Built message feature for shift swap request

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require("express")
-const app = express()
-const projectRoutes = require("./server/routes/ProjectRoutes")
-const scheduleRoutes = require("./server/routes/ScheduleRoutes")
-const swapShiftRoutes = require("./server/routes/swapShiftRoutes")
-const authRoutes = require("./server/routes/authRoutes")
-const userRoutes = require("./server/routes/userRoutes")
-const messageRoutes = require("./server/routes/messageRoutes")
-const calendarEventsRoutes = require("./server/routes/calendarEventsRoutes")
+const express = require("express");
+const app = express();
+const projectRoutes = require("./server/routes/ProjectRoutes");
+const scheduleRoutes = require("./server/routes/ScheduleRoutes");
+const swapShiftRoutes = require("./server/routes/swapShiftRoutes");
+const authRoutes = require("./server/routes/authRoutes");
+const userRoutes = require("./server/routes/userRoutes");
+const messageRoutes = require("./server/routes/messageRoutes");
+const calendarEventsRoutes = require("./server/routes/calendarEventsRoutes");
 
 const passport = require("passport");
 const session = require("express-session");
-const { updateMessages, findStoreAdmins } = require("./model/messaging");
+const { updateMessages, findStoreAdmins, sendAdminsShiftSwapRequest } = require("./model/messaging");
 app.use(session({ secret: "apple", cookie: { maxAge: 60000 } }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -18,12 +18,12 @@ app.use(passport.session());
 app.use(express.json());
 app.use("/api", projectRoutes);
 
-app.use("/api", authRoutes)
-app.use("/api/schedule", scheduleRoutes)
-app.use("/api/swapShift", swapShiftRoutes)
-app.use("/api", userRoutes)
-app.use("/api", messageRoutes)
-app.use("/api", calendarEventsRoutes)
+app.use("/api", authRoutes);
+app.use("/api/schedule", scheduleRoutes);
+app.use("/api/swapShift", swapShiftRoutes);
+app.use("/api", userRoutes);
+app.use("/api", messageRoutes);
+app.use("/api", calendarEventsRoutes);
 
 const PORT = 4000;
 function echoPortNumber() {
@@ -67,8 +67,15 @@ io.sockets.on("connect", (socket) => {
 
   socket.on("findStoreAdmins", (data) => {
     console.log("socket findStoreAdmins", data);
-    findStoreAdmins(JSON.parse(data))
-    console.log("socket admins", data);
+    findStoreAdmins(JSON.parse(data)).then((admins) => {
+      console.log("socket admins", admins);
+      socket.emit("storeAdmins", admins);
+    });
+  });
+
+  socket.on("shiftSwapMessage", (data) => {
+    console.log("socket shiftSwapMessage", data);
+    sendAdminsShiftSwapRequest(data)
   });
 
   // Leave the room if the user closes the socket
